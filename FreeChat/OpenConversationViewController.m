d07392eb8528fdedad2635b60575c16228022f66
junwen <jwfing@gmail.com>
2015-03-26 13:56:55 +0800

    refresh member count of open conversation

--- FreeChat/OpenConversationViewController.m
+++ FreeChat/OpenConversationViewController.m
@@ -12,11 +12,14 @@
 #import "ChatViewController.h"
 #import "MJRefresh.h"
 
-NSString * kOpenConversationCellIdentifier = @"OpenConversationIdentifier";
+NSString *kOpenConversationCellIdentifier = @"OpenConversationIdentifier";
+NSString *kConversationStatusFormat = @"%@(在线: %d 人)";
 
 @interface OpenConversationViewController () <UITableViewDataSource, UITableViewDelegate>{
     NSMutableArray *_conversations;
+    NSMutableDictionary *_memberCountDict;
     MJRefreshHeaderView *_refreshHead;
+    NSTimer *_memberCounterTimer;
 }
 
 @property (strong) UILabel *comment;
@@ -45,6 +48,18 @@ NSString * kOpenConversationCellIdentifier = @"OpenConversationIdentifier";
     [self addRefreshViews];
     
     [self refreshOpenConversations:NO];
+    _memberCountDict = [[NSMutableDictionary alloc] initWithCapacity:10];
+
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    _memberCounterTimer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(refreshConversationMembers) userInfo:nil repeats:YES];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [_memberCounterTimer invalidate];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -67,7 +82,6 @@ NSString * kOpenConversationCellIdentifier = @"OpenConversationIdentifier";
             [_refreshHead endRefreshing];
         }
     }];
-
 }
 
 - (void)addRefreshViews {
@@ -77,6 +91,27 @@ NSString * kOpenConversationCellIdentifier = @"OpenConversationIdentifier";
     _refreshHead.beginRefreshingBlock = ^(MJRefreshBaseView *refreshView) {
         [weakSelf refreshOpenConversations:YES];
     };
+}
+
+- (void)refreshConversationMembers {
+    NSLog(@"begin to refresh conversation member count...");
+    dispatch_group_t refreshGroup = dispatch_group_create();
+    int total = _conversations.count;
+    for (int i = 0; i < total; i++) {
+        AVIMConversation *conv = _conversations[i];
+        dispatch_group_enter(refreshGroup);
+        [conv countMembersWithCallback:^(NSInteger number, NSError *error) {
+            if (!error) {
+                [_memberCountDict setObject:[NSNumber numberWithInt:number] forKey:conv.name];
+            }
+            NSLog(@"countMembersWithCallback - %@ count: %d", conv.name, number);
+            dispatch_group_leave(refreshGroup);
+        }];
+    }
+    dispatch_group_notify(refreshGroup, dispatch_get_main_queue(), ^{
+        NSLog(@"end refresh conversation member count.");
+        [_refreshHead endRefreshing];
+    });
 }
 
 /*
@@ -94,9 +129,6 @@ NSString * kOpenConversationCellIdentifier = @"OpenConversationIdentifier";
     return [_conversations count];
 }
 
-// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.row >= _conversations.count) {
         return nil;
@@ -106,7 +138,8 @@ NSString * kOpenConversationCellIdentifier = @"OpenConversationIdentifier";
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kOpenConversationCellIdentifier];
     }
-    [cell.textLabel setText:targetConversation.name];
+    NSNumber *number = [_memberCountDict objectForKey:targetConversation.name];
+    [cell.textLabel setText:[NSString stringWithFormat:kConversationStatusFormat, targetConversation.name, number.intValue]];
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     return cell;
 }
